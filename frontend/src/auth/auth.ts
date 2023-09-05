83877d0e17a65266d6e3720c8a950ccea76706fd
Lucía <48994550+Palomox@users.noreply.github.com>
2023-09-05 18:20:42 +0200

    Ory Testing

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -6,7 +6,7 @@ export let session = ref<Session|undefined>(undefined)
 export let logoutUrl = ref("")
 
 export let ory = new FrontendApi({
-    basePath: "/.ory",
+    basePath: "https://romantic-satoshi-kojdtfzsl2.projects.oryapis.com",
     isJsonMime: (mime: string) => {return mime == "application/json"},
     baseOptions: {
         includeCredentials: true

90a1b0062f9ecb16f457fbeaf955863941c415ac
Lucía <48994550+Palomox@users.noreply.github.com>
2023-09-09 19:39:30 +0200

    Added Login form in page, changed router system

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -6,7 +6,7 @@ export let session = ref<Session|undefined>(undefined)
 export let logoutUrl = ref("")
 
 export let ory = new FrontendApi({
-    basePath: "/auth",
+    basePath: "/.ory",
     isJsonMime: (mime: string) => {return mime == "application/json"},
     baseOptions: {
         withCredentials: true
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import {library} from "@fortawesome/fontawesome-svg-core";
 import {faCircleXmark} from "@fortawesome/free-regular-svg-icons";
 import {useEventStore} from "@/stores/eventstore";
 import type {Session} from "@ory/client";
+import {logoutUrl, ory, session} from "@/auth/auth";
 
 
 
@@ -14,6 +15,25 @@ const pinia = createPinia();
 
 
 library.add(faCircleXmark)
+
+router.beforeEach(async (to, from) => {
+    if (to.path !== "/login") {
+        if (session.value == undefined) {
+            try {
+                let result = await ory.toSession()
+                session.value = result.data
+
+                ory.createBrowserLogoutFlow().then(({data}) => {
+                    logoutUrl.value = data.logout_url + "&return_to=" + window.location.origin
+                })
+            } catch (e) {
+                return {name: "Login"}
+
+            }
+        }
+    }
+})
+
 
 app.use(router);
 app.use(pinia);

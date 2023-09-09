f3ace0b0ede719942ef8e6f3a72bdd7a33bc87c6
Lucía <48994550+Palomox@users.noreply.github.com>
2023-09-09 21:41:37 +0200

    Created function cookie rewrite

--- frontend/api/handler.ts
+++ frontend/api/handler.ts
@@ -1,12 +1,25 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import {createProxyMiddleware} from "http-proxy-middleware";
 
+
+const proxy = createProxyMiddleware({
+    target: "https://romantic-satoshi-kojdtfzsl2.projects.oryapis.com",
+    changeOrigin: true,
+    pathRewrite: {
+        "^/.ory": "" // strip .ory from the url
+    },
+    onProxyRes(proxyRes){
+        let cookiesSet = proxyRes.headers["set-cookie"]
+
+        for(let cookie of cookiesSet){
+            console.log(cookie)
+            cookie.replace("romantic-satoshi-kojdtfzsl2.projects.oryapis.com", "calendarium.vercel.app")
+        }
+    }
+})
 export default function handler(
     request: VercelRequest,
     response: VercelResponse,
 ) {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
+    proxy(request, response)
 }

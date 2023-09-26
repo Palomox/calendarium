b33dcb2887ea8330d1991dc80170addee8b786e3
Lucía <48994550+Palomox@users.noreply.github.com>
2023-09-26 19:37:01 +0200

    [frontend] Aesthetic bugfixes
    + Fixed aesthetic bug in the edit period / event type view.
    + Cleaned up the styles a bit
    
    Signed-off-by: Lucía <48994550+Palomox@users.noreply.github.com>

--- frontend/src/views/EditPeriodsView.vue
+++ frontend/src/views/EditPeriodsView.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex flex-col w-1/2 m-auto mt-16 gap-4">
-    <display-period-component v-for="period of useEventStore().periods.periods" :key="period.name+period.startdate" :period="period"/>
-    <button @click="addNewPeriod()" class="w-16 h-16 self-center mt-8 p-2 text-5xl regular-button">
-      <font-awesome-icon icon="fa-solid fa-plus" />
-    </button>
+  <div class="w-1/2">
+    <div class="flex flex-col mt-32 gap-4">
+      <h1 class="text-7xl text-center font-bold mb-3">Periodos:</h1>
+      <display-period-component v-for="period of useEventStore().periods.periods" :key="period.name+period.startdate" :period="period"/>
+      <button @click="addNewPeriod()" class="w-16 h-16 self-center mt-8 p-2 text-5xl regular-button">
+        <font-awesome-icon icon="fa-solid fa-plus" />
+      </button>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import {onBeforeMount, ref} from "vue";
+import {onBeforeMount} from "vue";
 import {useEventStore} from "@/stores/eventstore";
 import DisplayPeriodComponent from "@/components/DisplayPeriodComponent.vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";

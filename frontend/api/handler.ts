4ce26c421377dbea11c4ab3bcd640c99e8ef03e5
Lucía <48994550+Palomox@users.noreply.github.com>
2024-01-19 13:52:24 +0100

    [frontend] Making handler be module exports syntax
    Signed-off-by: Lucía <48994550+Palomox@users.noreply.github.com>

--- frontend/api/handler.ts
+++ frontend/api/handler.ts
@@ -3,10 +3,12 @@ import {proxy} from "../src/proxy/oryProxy";
 
 const localDomain = process.env.VERCEL_ENV != 'production' ? process.env.VERCEL_URL : "calendarium.vercel.app";
 const oryDomain = process.env.VITE_ORY_DOMAIN;
-export default function handler(
+function handler(
     request: VercelRequest,
     response: VercelResponse,
 ) {
     // @ts-ignore
     proxy(request, response, localDomain, oryDomain)
 }
+
+module.exports = {handler}

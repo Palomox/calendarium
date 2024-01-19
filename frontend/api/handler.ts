b7cd87b97a3747fd85c5c75111a9eb91b9c6ebd8
Lucía <48994550+Palomox@users.noreply.github.com>
2024-01-19 15:28:03 +0100

    [frontend] Testing
    Signed-off-by: Lucía <48994550+Palomox@users.noreply.github.com>

--- frontend/api/handler.ts
+++ frontend/api/handler.ts
@@ -1,6 +1,12 @@
 import {VercelRequest, VercelResponse} from "@vercel/node";
 import {proxy} from "../src/proxy/oryProxy";
 
+
+export const config = {
+    runtime: 'nodejs',
+};
+
+
 const localDomain = process.env.VERCEL_ENV != 'production' ? process.env.VERCEL_URL : "calendarium.vercel.app";
 const oryDomain = process.env.VITE_ORY_DOMAIN;
 function handler(

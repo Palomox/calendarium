c216eb7c7d037381ecf3381bba0f996e3d08a4c1
Lucía <48994550+Palomox@users.noreply.github.com>
2024-01-19 16:29:07 +0100

    [frontend] added node polyfills to fix bug
    Signed-off-by: Lucía <48994550+Palomox@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    nodePolyfills()
   ],
   resolve: {
     alias: {
